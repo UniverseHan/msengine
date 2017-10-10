cb23b9332162a046b97e01cb579ac22f27dd5006
juyoungmoon <moon7313@douzone.com>
2017-10-10 16:09:06 +0900

    - Project 화면 표시

---
+++ client/src/project.js
@@ -0,0 +1,23 @@
+var Project = function (name, description, writer, duration, recruitment) {
+    this.name = name;
+    this.description = description;
+    this.writer = writer;
+    this.duration = duration;
+    this.recruitment = recruitment;
+};
+
+window.onload = function () {
+    var sampleProject = new Project("sample", "this is a sample", "sampler", "20171010~20171231", 3);
+    var projectDom = document.getElementById("project");
+
+    for(var i = 0; i < 3; i++) {
+        var projectNode = document.createElement("p");
+        projectDom.appendChild(projectNode);
+    }
+
+    console.log(projectDom.innerHTML);
+
+    projectDom.getElementsByTagName('p')[0].innerText = sampleProject.name;
+    projectDom.getElementsByTagName('p')[1].innerText = sampleProject.writer;
+    projectDom.getElementsByTagName('p')[2].innerText = sampleProject.recruitment;
+};
