5802732e5ff289559248a6a677518eaafdb07c68
universehan <hanuri7421@naver.com>
2017-10-21 07:56:42 +0000

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
+#!bin/bash
+cd `dirname $0`
+
 git pull
-./build.sh
-./run.sh
+docker build -t universehan/msengine .
+docker stop msengine-server
+docker rm msengine-server
+docker run -d --name msengine-server -p 8000:3000 universehan/msengine
