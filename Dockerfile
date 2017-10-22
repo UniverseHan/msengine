9c2c85afb81647c31eb293e013c7831968c9ca46
hanwool <hanuri7421@naver.com>
2017-10-22 21:42:00 +0900

    build client and deploy to public

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,14 @@ COPY . ~/msengine
 WORKDIR ~/msengine
 
 RUN npm install
+RUN cd client
+
+WORKDIR ~/msengine/client
+RUN npm install
+RUN npm run build
+
+WORKDIR ~/msengine
+RUN cp -rf client/dist public
+
 
 CMD node app.js
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var app = express();
 
-app.use(express.static('client'));
+app.use(express.static('public'));
 app.listen(3000, function () {
   console.log('Server has been started.');
 });
